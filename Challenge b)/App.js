5f50e40545a528c95fbe208edccb5710a5045ad7
OpungoJacob <145338644+OpungoJacob@users.noreply.github.com>
2023-10-31 22:55:34 +0300

    Update App.js

--- Challenge b)/App.js
+++ Challenge b)/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Navbar from './components/Navbar';
-import Main from './components/Main'
+import Main from './components/Main';
 
 export default function App(){
     return(
