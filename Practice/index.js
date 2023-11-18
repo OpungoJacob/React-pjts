d4ed05790f4b26180c629e952ebf174f4619dbc5
OpungoJacob <145338644+OpungoJacob@users.noreply.github.com>
2023-11-18 16:26:21 +0300

    Update index.js

--- Practice/index.js
+++ Practice/index.js
@@ -192,7 +192,7 @@ ReactDOM.render(
 /* Challenge:
 
 Part 2:
-- Add a `header` element with a nested `nav` element. inside the `nav`,
+i) - Add a `header` element with a nested `nav` element. inside the `nav`,
   include a `img` element with the image of the react logo inside
   (src="./react-logo.png") and make sure to set the width to something
   more manageable so it doesn't take up the whole screen.
