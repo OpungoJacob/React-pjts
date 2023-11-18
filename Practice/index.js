f57504556c4018c706e298cd80acb9a7aeb4506f
OpungoJacob <145338644+OpungoJacob@users.noreply.github.com>
2023-11-18 16:19:53 +0300

    Update index.js

--- Practice/index.js
+++ Practice/index.js
@@ -46,7 +46,7 @@ ReactDOM.render( */
 
 /* Challenge
 1. create a navbar in jsx:
--use the semantic `nav` element as the parent wrapper
+2. -use the semantic `nav` element as the parent wrapper
 -Have an h1 element with the brand name of your "website"
 -Insert an unordered list for the other nav elements
 -inside the `ul`, have three `li`s for "Pricing", "About", and "Contact"
