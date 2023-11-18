e127fdeee48ce4bc34789b0042e6c2ca939fa03d
OpungoJacob <145338644+OpungoJacob@users.noreply.github.com>
2023-11-18 16:23:46 +0300

    Update index.js

--- Practice/index.js
+++ Practice/index.js
@@ -75,7 +75,7 @@ ReactDOM.render( */
         
         1. Create a simple page in JSX (>= 4 elements) and save them in a variable
         2. Select the div with the ID of "root" and use `.append()` to append your JSX
-        3.See if you can guess what will show up in the browser before running the code
+        3. See if you can guess what will show up in the browser before running the code
         4.See if you can explain what exactly shows up in the browser
     
 
