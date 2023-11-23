e93e3afc0ed2d9d1c55d45dc3de602bd0c52d72f
OpungoJacob <145338644+OpungoJacob@users.noreply.github.com>
2023-11-23 17:10:13 +0300

    Update Login.js

--- Signu-signin -v1/src/pages/Login.js
+++ Signu-signin -v1/src/pages/Login.js
@@ -1,7 +1,7 @@
 import React from "react";
 import {Link} from 'react-router-dom';
 import {useState} from "react";
-import axios from "axios"
+import axios from "axios";
 import {useNavigate} from "react-router-dom"
 
 export default function Login(){
