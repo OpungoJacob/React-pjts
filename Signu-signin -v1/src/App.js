325fa533f789f0ec92678278a889f826bd76dd05
OpungoJacob <145338644+OpungoJacob@users.noreply.github.com>
2023-11-23 17:08:35 +0300

    Update App.js

--- Signu-signin -v1/src/App.js
+++ Signu-signin -v1/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import {BrowserRouter, Routes, Route} from "react-router-dom";
-import Signup from "./pages/Signup"
+import Signup from "./pages/Signup";
 import Login from "./pages/Login"
 import Home from "./pages/Home"
 import 'bootstrap/dist/css/bootstrap.min.css'
