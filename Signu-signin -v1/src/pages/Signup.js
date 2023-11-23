6a3f4cc04195dc96c81ccf0459c665c8974d4f92
OpungoJacob <145338644+OpungoJacob@users.noreply.github.com>
2023-11-23 17:11:08 +0300

    Update Signup.js

--- Signu-signin -v1/src/pages/Signup.js
+++ Signu-signin -v1/src/pages/Signup.js
@@ -2,7 +2,7 @@ import React from "react";
 import {Link} from 'react-router-dom';
 import { useState } from "react"
 import axios from 'axios';
-import {useNavigate} from "react-router-dom"
+import {useNavigate} from "react-router-dom";
 
 export default function Signup(){
     const [surname, setSurname] = useState()
