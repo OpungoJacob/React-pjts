40991e049b3badd32fb5da806bcee32937a31cbb
OpungoJacob <145338644+OpungoJacob@users.noreply.github.com>
2023-11-26 11:44:36 +0300

    Update server.js

--- Workout site/server/server.js
+++ Workout site/server/server.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 
 const express = require('express');
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 const workoutRoutes = require('./routes/workouts')
 
 //express app
