b1527b6db07d93a91bbdd178b17c6a65b79339ca
OpungoJacob <145338644+OpungoJacob@users.noreply.github.com>
2023-12-01 00:12:04 +0300

    Update server.js

--- Signu-signin -v1/server/server.js
+++ Signu-signin -v1/server/server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const EmployeeModel = require('./models/Employee');
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
 app.use(cors())
 
 mongoose.connect("mongodb://127.0.0.1:27017/employee");
